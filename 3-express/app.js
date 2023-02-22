815aa07507fd60e585fd5689116757c2993dc21f
pformela <patrykformela.00@gmail.com>
2023-02-22 15:21:52 +0100

    finished handling express errors section

--- 3-express/app.js
+++ 3-express/app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
+
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 
@@ -9,12 +12,18 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
-app.use((req, res, next) => {
-  console.log("Hello from the middleware");
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log("Hello from the middleware");
+//   next();
+// });
 
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
--- 3-express/server.js
+++ 3-express/server.js
@@ -18,6 +18,22 @@ mongoose
   });
 
 const port = 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Listening on port ${port}`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log("UNHANDLED REJECTION! Shutting down...");
+  console.log(err);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on("uncaughtException", (err) => {
+  console.log("UNCAUGHT EXCEPTION! Shutting down...");
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
